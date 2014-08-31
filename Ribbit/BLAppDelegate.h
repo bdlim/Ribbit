3d35890839262e3c5715c7f6ef33b8cae1198c1a
Brandon Lim <bdlim2004@gmail.com>
2014-08-31 15:22:34 -0700

    Initial Commit

---
+++ Ribbit/BLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLAppDelegate.h
+//  Ribbit
+//
+//  Created by Brandon Lim on 8/31/14.
+//  Copyright (c) 2014 Brandon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Brandon Lim on 8/31/14.
+//  Copyright (c) 2014 Brandon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLAppDelegate class]));
+    }
+}
